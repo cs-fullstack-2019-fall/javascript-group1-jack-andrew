b879114e06225d299d2ed33cbc1191476ce18769
jackbolton901 <bltnjck@gmail.com>
2019-08-20 10:34:56 -0500

    functions

--- person1.js
+++ person1.js
@@ -1,3 +1,16 @@
 let engArray= ['Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
 let spanArray=['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
 
+function engToSpan()
+{
+    for(index=0;index< engArray.length; index++){
+        console.log(`${engArray[index]}- ${spanArray[index]}`)
+    }
+}
+
+function spanToEng()
+{
+    for(index=0;index<spanArray.length; index++){
+        console.log(`${spanArray[index]}- ${engArray[index]}`)
+    }
+}
