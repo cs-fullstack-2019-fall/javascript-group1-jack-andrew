758a10b61cbcb7602e7ac29294dc08d894815c82
jackbolton901 <bltnjck@gmail.com>
2019-08-20 10:51:49 -0500

    adding flavor

--- person1.js
+++ person1.js
@@ -1,16 +1,16 @@
 let engArray= ['Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
 let spanArray=['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
-
+// function to translate english to spanish
 function engToSpan()
 {
     for(index=0;index< engArray.length; index++){
-        console.log(`${engArray[index]}- ${spanArray[index]}`)
+        console.log(`The Spanish word for ${engArray[index]}- is ${spanArray[index]}.`);
     }
 }
-
+// function to translate spanish to English
 function spanToEng()
 {
     for(index=0;index<spanArray.length; index++){
-        console.log(`${spanArray[index]}- ${engArray[index]}`)
+        console.log(`The English word for ${spanArray[index]} is ${engArray[index]}.`);
     }
 }
